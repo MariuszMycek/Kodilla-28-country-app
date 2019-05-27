6fe1b37b03e33e269d8b8f87cf4164381c621dc6
Mariusz Mycek <m.mycek83@gmail.com>
2019-05-27 13:54:56 +0200

    Finish app - database of countries synchronized with Redux

--- src/presentational/flag.component.js
+++ src/presentational/flag.component.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const CountryFlag = props => (
   <div className='country-logo-wrapper'>
-    <img
-      src={props.country.imageUrl}
-      alt='country'
-      className='country-logo'
-    />
+    <img src={props.country.imageUrl} alt='country' className='country-logo' />
   </div>
 );
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
-// import { getCountries } from '../actions/actions-countries';
 
 const store = createStore(
   reducers,
